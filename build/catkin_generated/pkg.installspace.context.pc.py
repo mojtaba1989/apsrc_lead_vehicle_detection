51a5bd082c8c034feb37d9bac93655f28c373df9
Mojtaba <46693665+mojtaba1989@users.noreply.github.com>
2024-03-18 09:30:58 -0400

    Delete build directory

--- build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "apsrc_msgs;autoware_msgs;geometry_msgs;nodelet;roscpp;roslib;roslint;std_msgs;radar_msgs;derived_object_msgs;tf2_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lapsrc_lead_vehicle_detection_nodelets".split(';') if "-lapsrc_lead_vehicle_detection_nodelets" != "" else []
-PROJECT_NAME = "apsrc_lead_vehicle_detection"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "1.0.0"
--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/moji/apsrc_ws/src/apsrc_lead_vehicle_detection/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
